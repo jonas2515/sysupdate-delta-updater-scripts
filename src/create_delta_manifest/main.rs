3f3cb6c5abfdf40885cb573572289c97cd07f0e4
Jonas Dreßler <verdre@v0yd.nl>
2025-12-09 02:45:12 +0100

    Factor out more things into delta_manifest module

--- src/create_delta_manifest/main.rs
+++ src/create_delta_manifest/main.rs
@@ -3,54 +3,14 @@ use std::io::{self, Read};
 use std::env;
 use std::process;
 use std::error::Error;
-//use std::collections::HashMap;
-use crc_fast::{checksum, CrcAlgorithm::Crc64Nvme};
-use std::process::Command;
 use gvariant::{gv, Marker};
 
-const BLOCK_SIZE: usize = 4096;
-
-struct Manifest {
-  version: u32,           /* manifest version */
-  verity_salt: [u8; 32],  /* salt to use for dm-verity */
-  image_hash: [u8; 32],   /* sha256 hash of the image (unsalted) */
-  hash_blocks: Vec<u64>,  /* block hashes crc64 */
-}
+use sysupdate_delta_updater_scripts::delta_manifest;
 
 fn help() {
     println!("usage: create_delta_manifest <image>
 
 Create a delta update manifest for the image file <image>.");
-}
-
-fn read_image(filename: &str) -> Result<(usize, Vec<u64>), Box<dyn Error>> {
-    let mut image = File::open(filename)?;
-    let mut buffer = [0u8; BLOCK_SIZE];
-    let mut total_bytes_read = 0;
-    let mut hashes = Vec::new();
-
-    loop {
-        let bytes_read = image.read(&mut buffer)?;
-        if bytes_read == 0 {
-            break;
-        }
-
-        total_bytes_read += bytes_read;
-        let _block_num = total_bytes_read / BLOCK_SIZE;
-
-        let block_hash = checksum(Crc64Nvme, &buffer.get(..bytes_read).ok_or("arr out of bounds")?);
-        hashes.push(block_hash);
-
-        if total_bytes_read % (1024 * 1024 * 100) == 0 {
-            println!("status: read {} MiB of file={}",
-                     total_bytes_read / (1024 * 1024), filename);
-        }
-    }
-
-    println!("Finished reading: file={}, total_bytes_read={} MiB",
-             filename, total_bytes_read / (1024 * 1024));
-
-    Ok((total_bytes_read, hashes))
 }
 
 fn generate_salt() -> Result<[u8; 32], io::Error> {
@@ -63,33 +23,21 @@ fn generate_salt() -> Result<[u8; 32], io::Error> {
     Ok(salt)
 }
 
-fn measure_sha256sum(filename: &str) -> Result<[u8; 32], Box<dyn Error>> {
-    // we call sha256sum/cksum externally because it's way faster than calculating it ourselves
-    let cmd_out = Command::new("cksum")
-        .arg("-a")
-        .arg("sha256")
-        .arg("--raw")
-        .arg(filename)
-        .output()?;
-
-    Ok(cmd_out.stdout.get(..).ok_or("arr out of bounds")?.try_into()?)
-}
-
 fn create_manifest(image: &str, manifest_filename: &str) -> Result<(), Box<dyn Error>> {
     let salt = generate_salt()?;
     println!("salt: {:?}", salt);
 
-    let sha256sum = measure_sha256sum(image)?;
+    let sha256sum = delta_manifest::measure_sha256sum(image)?;
     println!("sha256sum of image: {:?}", sha256sum);
 
-    let (_, block_hashes) = read_image(image)?;
+    let block_hashes = delta_manifest::read_image_block_hashes(image)?;
     println!("number of block hashes: {}", block_hashes.len());
 
-    let manifest = Manifest {
+    let manifest = delta_manifest::Manifest {
         version: 1,
         verity_salt: salt,
         image_hash: sha256sum,
-        hash_blocks: block_hashes,
+        block_hashes: block_hashes,
     };
 
     let mut manifest_file = File::create(manifest_filename)?;
@@ -97,7 +45,7 @@ fn create_manifest(image: &str, manifest_filename: &str) -> Result<(), Box<dyn E
         manifest.version,
         manifest.verity_salt,
         manifest.image_hash,
-        &manifest.hash_blocks,
+        &manifest.block_hashes,
     );
 
     gv!("(uayayat)").serialize(&manifest_as_tuple, &mut manifest_file)?;
--- src/delta_manifest/mod.rs
+++ src/delta_manifest/mod.rs
@@ -1,12 +1,12 @@
 use std::fs::{File};
-use std::env;
-use std::process;
 use std::error::Error;
-//use std::collections::HashMap;
-//use sha256::digest;
-//use sha2::{Sha256};
-use std::io::Read;
+use std::collections::HashMap;
+use std::io;
+use std::io::{Read, Seek};
 use gvariant::{gv, Marker, Structure, aligned_bytes::copy_to_align};
+use crc_fast::{checksum, CrcAlgorithm::Crc64Nvme};
+use bytes::Bytes;
+use std::process::Command;
 
 pub const BLOCK_SIZE: usize = 4096;
 
@@ -17,17 +17,9 @@ pub struct Manifest {
   pub block_hashes: Vec<u64>,  /* block hashes crc64 */
 }
 
-fn check_manifest_version(manifest_filename: &str) -> Result<(), Box<dyn Error>> {
-    let mut manifest_file = File::open(manifest_filename)?;
-
-    // read the first 4 bytes of the file first as a simple, forward-compatible version check
-    // FIXME: I think this shouldn't cause problems with endianness etc, but not 100% sure
-    let mut buffer = [0u8; 4];
-    let bytes_read = manifest_file.read(&mut buffer)?;
-    if bytes_read != 4 {
-        return Err("Failed to read manifest version".into())
-    }
-    let buffer = copy_to_align(&buffer);
+fn check_manifest_version(manifest_version: &[u8]) -> Result<(), Box<dyn Error>> {
+    assert!(manifest_version.len() == 4);
+    let buffer = copy_to_align(manifest_version);
     let manifest_version = gv!("u").cast(&buffer);
     if *manifest_version != 1 {
         return Err(format!("Unsupported manifest version: {}", manifest_version).into())
@@ -37,9 +29,19 @@ fn check_manifest_version(manifest_filename: &str) -> Result<(), Box<dyn Error>>
 }
 
 pub fn read_manifest(manifest_filename: &str) -> Result<Manifest, Box<dyn Error>> {
-    check_manifest_version(manifest_filename)?;
+    // read the first 4 bytes of the file first as a simple, forward-compatible version check
+    // FIXME: I think this shouldn't cause problems with endianness etc, but not 100% sure
+    let mut manifest_file = File::open(manifest_filename)?;
 
-    let manifest_file = File::open(manifest_filename)?;
+    let mut buffer = [0u8; 4];
+    let bytes_read = manifest_file.read(&mut buffer)?;
+    if bytes_read != 4 {
+        return Err("Failed to read manifest version".into())
+    }
+    check_manifest_version(&buffer)?;
+
+    manifest_file.rewind()?;
+
     let variant = gv!("(uayayat)").deserialize(manifest_file)?;
     let manifest_as_tuple = variant.to_tuple();
 
@@ -51,4 +53,92 @@ pub fn read_manifest(manifest_filename: &str) -> Result<Manifest, Box<dyn Error>
     };
 
     Ok(manifest)
+}
+
+pub fn read_manifest_bytes(manifest_bytes: Bytes) -> Result<Manifest, Box<dyn Error>> {
+    check_manifest_version(&manifest_bytes[0..4])?;
+
+    let buffer = copy_to_align(&manifest_bytes);
+    let variant = gv!("(uayayat)").cast(&buffer);
+    let manifest_as_tuple = variant.to_tuple();
+
+    let manifest = Manifest {
+        version: *manifest_as_tuple.0,
+        verity_salt: manifest_as_tuple.1.try_into()?,
+        image_hash: manifest_as_tuple.2.try_into()?,
+        block_hashes: manifest_as_tuple.3.to_vec(),
+    };
+
+    Ok(manifest)
+}
+
+pub fn read_image_block_hashes(filename: &str) -> Result<Vec<u64>, io::Error> {
+    let mut image = File::open(filename)?;
+    let mut buffer = [0; BLOCK_SIZE];
+    let mut total_bytes_read = 0;
+    let mut blocks_to_hashes = Vec::new();
+
+    loop {
+        let bytes_read = image.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+
+        total_bytes_read += bytes_read;
+
+        let block_hash = checksum(Crc64Nvme, &buffer[..bytes_read]);
+        blocks_to_hashes.push(block_hash);
+
+        if total_bytes_read % (1024 * 1024 * 100) == 0 {
+            println!("status: read {} MiB of file={}",
+                     total_bytes_read / (1024 * 1024), filename);
+        }
+    }
+
+    println!("Finished reading: file={}, total_bytes_read={} MiB",
+             filename, total_bytes_read / (1024 * 1024));
+
+    Ok(blocks_to_hashes)
+}
+
+pub fn hashes_to_blocks_map_from_block_hashes(block_hashes: Vec<u64>) -> HashMap<u64, usize> {
+    let mut hashes_to_blocks = HashMap::new();
+
+    for (block_num, hash) in block_hashes.iter().enumerate() {
+        hashes_to_blocks.insert(*hash, block_num);
+    }
+
+    hashes_to_blocks
+}
+
+// this creates an array where the index is the block index in the new_image,
+// and the value is either:
+// 0: in case the block is not present in the old_image
+// or otherwise, it's the block num in the old_image
+pub fn get_new_to_old_block_mapping(old_image_hashes_to_blocks: HashMap<u64, usize>, new_image_blocks_to_hashes: Vec<u64>) -> (usize, Vec<u64>) {
+    let mut new_blocks_to_old_blocks: Vec<u64> = Vec::new();
+    let mut n_blocks_avail_in_old_image = 0;
+
+    for hash in new_image_blocks_to_hashes.iter() {
+        if let Some(block_num_in_old_image) = old_image_hashes_to_blocks.get(hash) {
+            new_blocks_to_old_blocks.push(*block_num_in_old_image as u64);
+            n_blocks_avail_in_old_image += 1;
+        } else {
+            new_blocks_to_old_blocks.push(0);
+        }
+    }
+
+    (n_blocks_avail_in_old_image, new_blocks_to_old_blocks)
+}
+
+pub fn measure_sha256sum(filename: &str) -> Result<[u8; 32], Box<dyn Error>> {
+    // we call sha256sum/cksum externally because it's way faster than calculating it ourselves
+    let cmd_out = Command::new("cksum")
+        .arg("-a")
+        .arg("sha256")
+        .arg("--raw")
+        .arg(filename)
+        .output()?;
+
+    Ok(cmd_out.stdout.get(..).ok_or("arr out of bounds")?.try_into()?)
 }
