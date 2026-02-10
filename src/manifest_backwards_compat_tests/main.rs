6791809435ae0fbf817f855a19c8f88f9ddfe6c2
Jonas Dreßler <verdre@v0yd.nl>
2026-02-11 00:40:57 +0100

    manifest_backwards_compat_tests: Add experiment using a vardict for the manifests
    This seems like the nicer approach for manifest and forward+backward
    compatibility: Instead of specifying a version number in the manifest,
    we simply make it a gvariant vardict.
    
    This means we profit from the space efficiency of gvariant, while being
    able to use the vardict for ensuring forward+backwards compatibility.
    To change the format while remaining backwards compatible, a new (optional)
    key would be added to the vardict, while the existing keys of the
    original format would remain mandatory.
    
    In case the format has to be reworked including breaking changes, we
    would use a different filename, aka ".manifest2" rather than ".manifest".

---
+++ src/manifest_backwards_compat_tests/main.rs
@@ -0,0 +1,56 @@
+use serde::{Deserialize, Serialize};
+use zvariant::{
+    LE, to_bytes, serialized::Context, Type,
+    signature::{Signature, Fields},
+    SerializeDict, DeserializeDict,
+    signature,
+    as_value,
+};
+
+fn main() {
+    let ctxt = Context::new_dbus(LE, 0);
+
+    // First specify a v1 version of the format and make sure that deserializes
+    // as expected.
+
+    #[derive(Debug, PartialEq, Type, Eq, Serialize, Deserialize)]
+    #[zvariant(signature = "a{sv}")]
+    struct Manifest {
+        #[serde(with = "as_value")]
+        pub block_hashes: Vec<u64>,
+    }
+
+    let test_manifest_v1 = Manifest {
+        block_hashes: [342, 39].to_vec(),
+    };
+
+    let encoded = to_bytes(ctxt, &test_manifest_v1).unwrap();
+    let decoded: Manifest = encoded.deserialize().unwrap().0;
+    assert_eq!(&decoded, &test_manifest_v1);
+
+    // Now specify a potential v2 version of the format, and add an optional
+    // new value to the vardict, while keeping the existing value as-is (this is
+    // how we keep it backwards compatible).
+
+    #[derive(Debug, PartialEq, Type, Eq, Serialize, Deserialize)]
+    #[zvariant(signature = "a{sv}")]
+    struct ManifestV2 {
+        #[serde(with = "as_value::optional", skip_serializing_if = "Option::is_none")]
+        pub block_hashes_v2: Option<Vec<i16>>,
+        #[serde(with = "as_value")]
+        pub block_hashes: Vec<u64>,
+    }
+
+    let test_manifest_v2 = ManifestV2 {
+        block_hashes_v2: Some([0x24, 100, 3, 381, 391, 9].to_vec()),
+        block_hashes: [0x186].to_vec(),
+    };
+
+    // Serialize, then deserialize to a Manifest v1 and assert that it
+    // deserializes fine to that type.
+    let encoded = to_bytes(ctxt, &test_manifest_v2).unwrap();
+    let decoded: Manifest = encoded.deserialize().unwrap().0;
+    assert_eq!(&decoded, &Manifest {
+        block_hashes: [0x186].to_vec(),
+    });
+}
